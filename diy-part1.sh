46ec835fc800d9602c061dfd5536ab69e630a34b
longshij <72343084+longshij@users.noreply.github.com>
2025-07-17 12:34:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
 # 添加 Passwall 插件源（主源和依赖）
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages" >> feeds.conf.default
+
+# 添加 MosDNS 插件源（若主源码已带，可不加）
+echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns" >> feeds.conf.default
+
