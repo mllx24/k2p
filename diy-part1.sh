85a9ee30f9e85a5aa6dfc1c1d40c179effcdc9ab
mllx24 <hz827595@outlook.com>
2025-07-24 15:14:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-# 添加 Passwall 插件源（主源和依赖）
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages" >> feeds.conf.default
-
-# 添加 MosDNS 插件源（若主源码已带，可不加）
-echo "src-git mosdns https://github.com/sbwml/luci-app-mosdns" >> feeds.conf.default
 
